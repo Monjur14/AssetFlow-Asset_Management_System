04f7a3c8d43fb2d40dda177910b3a6f4cd1c7f0c
Monjur Hossen <14monjurhossen@gmail.com>
2024-06-10 22:12:39 +0600

    asset list half complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -100,7 +100,7 @@ const Navbar = () => {
     }
     </div>
       </div>
-    }
+    }                
                                                                 
                                                                      
 
