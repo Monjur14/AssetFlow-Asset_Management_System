f92207de6164977e57aa7714e12690e8b19c6c11
Monjur Hossen <14monjurhossen@gmail.com>
2024-06-26 01:58:16 +0600

    Updatedddd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './Components/Navbar'
-// import Footer from './Components/Footer'
+import Footer from './Components/Footer'
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <div className='flex-1'>
           <Outlet/>
         </div>
-        {/* <Footer/> */}
+        <Footer/>
       </div>
     </div>
   )
